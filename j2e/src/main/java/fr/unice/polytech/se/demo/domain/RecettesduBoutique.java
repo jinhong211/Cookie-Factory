cea2c9e2f8763450375e753d962a64699fbf7c98
pol-wang <869084711@qq.com>
2015-04-27 21:10:29 +0200

    add RecettesduBoutique

---
+++ j2e/src/main/java/fr/unice/polytech/se/demo/domain/RecettesduBoutique.java
@@ -0,0 +1,14 @@
+package fr.unice.polytech.se.demo.domain;
+
+import fr.unice.polytech.se.demo.entities.Commande;
+
+import java.util.Set;
+
+/**
+ * Created by user on 27/04/15.
+ */
+public interface RecettesduBoutique {
+
+    Set<Commande> getRecettesduBoutique(String adresseBoutique);
+
+}
