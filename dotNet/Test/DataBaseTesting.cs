02f77dff9a95a859455d76d1b93d6bcff0971a8d
pol-jin <jinhong211@gmail.com>
2015-04-27 17:56:16 +0200

    test de la méthode de getListUtilisateurs()

---
+++ dotNet/Test/DataBaseTesting.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using DAO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Test
+{
+    [TestClass]
+    public class DataBaseTesting : DbTestBase
+    {
+        [TestMethod]
+        public void TestGet()
+        {   
+            var utilisateur = new UtilisateurDAO();
+            List<Utilisateur> utilisateurList = utilisateur.getListUtilisateurs();
+
+            Assert.IsNotNull(utilisateurList);
+        }
+    }
+}
---
+++ dotNet/Test/DbTestBase.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DAO;
+using System.Transactions;
+
+namespace Test
+{
+    [TestClass]
+    public class DbTestBase
+    {
+        private TransactionScope scope;   
+        [TestInitialize]
+        public void SetUp()
+        {
+            this.scope = new TransactionScope();
+        }
+       
+        [TestCleanup]
+        public void TearDown()
+        {
+            this.scope.Dispose();
+        }
+    }
+}
