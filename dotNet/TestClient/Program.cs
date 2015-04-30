b174306d2c896e718f5890e03003c4d5b8378594
pol-jin <jinhong211@gmail.com>
2015-04-30 22:45:15 +0200

    Les tests du client de Webservices.

--- dotNet/TestClient/Program.cs
+++ dotNet/TestClient/Program.cs
@@ -9,10 +9,37 @@ namespace TestClient
     {
         static void Main(string[] args)
         {
-            ServiceReference1.ServiceTCFClient userServiceStub = new
-ServiceReference1.ServiceTCFClient();
+            ServiceTCF.ServiceTCFClient userServiceStub = new
+ServiceTCF.ServiceTCFClient();
+            Console.WriteLine(userServiceStub.getListAccount());
             Console.WriteLine(userServiceStub.createAccount("dingding","dd123","USER"));
+            Console.WriteLine(userServiceStub.createAccount("dingding", "dd123", "USER"));
             Console.WriteLine(userServiceStub.createAccount("smallbirdking", "sbk123", "USER"));
+            Console.WriteLine(userServiceStub.deleteAccount("smallbirdking"));
+            Console.WriteLine(userServiceStub.deleteAccount("jinhong"));
+            Console.WriteLine(userServiceStub.updateAccount("jinhong","12345","ADMIN"));
+            Console.WriteLine(userServiceStub.updateAccount("dingding","dd1111","ADMIN"));
+            Console.WriteLine(userServiceStub.getListAccount());
+            Console.WriteLine(userServiceStub.loginAccount("dingding","dd123"));
+            Console.WriteLine(userServiceStub.loginAccount("dingding","dd1111"));
+            Console.WriteLine(userServiceStub.loginAccount("smallbirdking","sbk123"));
+            Console.WriteLine(userServiceStub.getListInfoAccount("dingding"));
+            Console.WriteLine(userServiceStub.getListInfoAccount("smallbirdking"));
+            Console.WriteLine(userServiceStub.addInfoAccount("dingding", "Ding", "Feng", 1234567, "Valbonne", "11/17", 123));
+            Console.WriteLine(userServiceStub.addInfoAccount("dingding", "Ding", "Feng", 1234567, "Valbonne", "11/17", 123));
+            Console.WriteLine(userServiceStub.addInfoAccount("dingding", "Ding", "Feng", 1111111, "Valbonne", "11/17", 123));
+            Console.WriteLine(userServiceStub.addInfoAccount("smallbirdking", "Wang", "Yuqi", 1234567, "Valbonne", "11/17", 123));
+            Console.WriteLine(userServiceStub.getInfoAccount("dingding",1111111));
+            Console.WriteLine(userServiceStub.getInfoAccount("dingding",1123212));
+            Console.WriteLine(userServiceStub.getInfoAccount("smallbirdking",1111111));
+            Console.WriteLine(userServiceStub.getListInfoAccount("dingding"));
+            Console.WriteLine(userServiceStub.deleteInfoAccount("dingding",1111111));
+            Console.WriteLine(userServiceStub.deleteInfoAccount("dingding",1111111));
+            Console.WriteLine(userServiceStub.deleteInfoAccount("smallbirdking",1111111));
+            Console.WriteLine(userServiceStub.updateInfoAccount("dingding",1234567,"Antibes"));
+            Console.WriteLine(userServiceStub.updateInfoAccount("dingding",1111111,"Antibes"));
+            Console.WriteLine(userServiceStub.updateInfoAccount("smallbirdking",1111111,"Antibes"));
+            Console.WriteLine(userServiceStub.getListInfoAccount("dingding"));
             Console.ReadLine();
         }
     }
