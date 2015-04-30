e84d1a4b9e6e8e6ebd7e47241a7be3553398bc3a
pol-jin <jinhong211@gmail.com>
2015-04-30 21:39:12 +0200

    webservice pour les opérations du compte

--- dotNet/WcfServiceTCF/IServiceTCF.cs
+++ dotNet/WcfServiceTCF/IServiceTCF.cs
@@ -13,5 +13,23 @@ namespace WcfServiceTCF
     {
         [OperationContract]
         String createAccount(String login, String passward, String type);
+        [OperationContract]
+        String deleteAccount(String login);
+        [OperationContract]
+        String updateAccount(String login,String passward,String type);
+        [OperationContract]
+        String loginAccount(String login, String passward);
+        [OperationContract]
+        String getListAccount();
+        [OperationContract]
+        String getListInfoAccount();
+        [OperationContract]
+        String addInfoAccount();
+        [OperationContract]
+        String deleteInfoAccount();
+        [OperationContract]
+        String updateInfoAccount();
+        [OperationContract]
+        String getInfoAccount();
     }
 }
--- dotNet/WcfServiceTCF/ServiceTCF.svc.cs
+++ dotNet/WcfServiceTCF/ServiceTCF.svc.cs
@@ -24,5 +24,67 @@ namespace WcfServiceTCF
                 return "User " + login + " exists";
             }
         }
+
+        public String deleteAccount(String login)
+        {
+            var dao = new UtilisateurDAO();
+            if (dao.getUtilisateur(login) != null)
+            {
+                dao.removeUtilisateur(login);
+                return "User " + login + " deleted";
+            }
+            else
+            {
+                return "User " + login + " not exists";
+            }
+        }
+
+        public String updateAccount(String login,String passward,String type)
+        {
+            var dao = new UtilisateurDAO();
+            if (dao.getUtilisateur(login) != null)
+            {
+                dao.modifyUtilisateur(login, passward, type);
+                return "User " + login + " updated";
+            }
+            else
+            {
+                return "User " + login + " not exists";
+            }
+        }
+
+        public String loginAccount(String login, String passward)
+        {
+            var dao = new UtilisateurDAO();
+            if (dao.getUtilisateur(login) != null)
+            {
+                if (passward == dao.getUtilisateur(login).passward)
+                {
+                    return "User " + login + " login successfully";
+                }
+                else
+                {
+                    return "Wrong passward.";
+                }
+            }
+            else
+            {
+                return "User " + login + " not exists";
+            }
+        }
+
+        public String getListAccount()
+        {
+            String result = "";
+            var dao = new UtilisateurDAO();
+            for (int i = 0; i < dao.getListUtilisateurs().Count<Utilisateur>(); i++)
+            {
+                result = result + "User: " + dao.getListUtilisateurs()[i].login;
+                result = result + " Passward: " + dao.getListUtilisateurs()[i].passward;
+                result = result + " Type: " + dao.getListUtilisateurs()[i].type;
+                result = result + "\n";
+            }
+                return result;
+        }
     }
 }
