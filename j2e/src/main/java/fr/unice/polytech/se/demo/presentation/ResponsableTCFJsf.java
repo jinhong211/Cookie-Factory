988eeb1f734e07caf7ea6658657b30ad1002ff5f
Feng DING <ding.feng@etu.unice.fr>
2015-05-12 01:34:15 +0200

    MAJ JSF responsableTCFCreerBoutiqueDisplay

--- j2e/src/main/java/fr/unice/polytech/se/demo/presentation/ResponsableTCFJsf.java
+++ j2e/src/main/java/fr/unice/polytech/se/demo/presentation/ResponsableTCFJsf.java
@@ -1,11 +1,15 @@
 package fr.unice.polytech.se.demo.presentation;
 
+import fr.unice.polytech.se.demo.domain.BoutiqueFinder;
 import fr.unice.polytech.se.demo.domain.CreerBoutique;
+import fr.unice.polytech.se.demo.entities.Boutique;
 
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by ding on 01/05/15.
@@ -16,6 +20,8 @@ public class ResponsableTCFJsf implements Serializable{
 
     @EJB
     private CreerBoutique creerBoutique;
+    @EJB
+    private BoutiqueFinder boutiqueFinder;
 
     private String adresseBoutique;
 
@@ -32,28 +38,53 @@ public class ResponsableTCFJsf implements Serializable{
         return adresseBoutique;
     }
 
-    public void setTax(String t) throws Exception{
-        this.tax = Double.parseDouble(t);
+    public void setTax(double t) throws Exception{
+        this.tax = t;
     }
 
-    public void setChiffreVent(String cv) throws Exception{
-        this.tax = Integer.parseInt(cv);
+    public double getTax() throws Exception{
+        return tax;
     }
 
-    public void setTempsO(String to) throws Exception{
-        this.tempsO = Integer.parseInt(to);
+    public void setTempsO(int to) throws Exception{
+        this.tempsO = to;
     }
 
-    public void setTempsF(String tf) throws Exception{
-        this.tempsF = Integer.parseInt(tf);
+    public void setTempsF(int tf) throws Exception{
+        this.tempsF = tf;
     }
 
-    public void setAdresseBoutique(){
-        creerBoutique.createBoutique(adresseBoutique,tax,chiffreVent,tempsO,tempsF);
+    public int getTempsO(){
+        return tempsO;
     }
 
-    public String getChiffreVent(){
-        return "" + chiffreVent;
+    public int getTempsF(){
+        return tempsF;
+    }
+
+    public void creerBoutique(){
+        creerBoutique.createBoutique(adresseBoutique, tax, 0, tempsO, tempsF);
+    }
+
+    public ArrayList<String> getAllBoutique(){
+        String s = "";
+        ArrayList<String> ss = new ArrayList<String>();
+        creerBoutique.createBoutique("Polytech",2.2,0,6,10);
+        creerBoutique.createBoutique("Antibes",1.3,0,10,12);
+        List<Boutique> boutiques = boutiqueFinder.findAll();
+        for(Boutique b : boutiques){
+            s += b.getAddresseBoutique();
+            ss.add(b.getAddresseBoutique());
+        }
+        return ss;
+    }
+
+    public int getChiffreVent(){
+        return chiffreVent;
+    }
+
+    public void setChiffreVent(int cv) throws Exception{
+        this.chiffreVent = cv;
     }
 
 
